154bf905acc8bdf996e983beddee79c93ecdf614
SergeyBordulev <bordulev@yahoo.com>
2021-10-25 14:16:26 +0700

    Initial commit

---
+++ src/SiteChecker.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+
+public class SiteChecker {
+    public static void main(String[] args) throws IOException {
+        String[] modelArray = {"bmw m3"};
+        String[] cityArray = {"atlanta", "austin", "boston", "chicago", "dallas", "denver", "detroit", "houston", "lasvegas", "losangeles", "miami", "minneapolis", "newyork", "philadelphia", "phoenix", "portland", "raleigh", "sacramento", "sandiego", "seattle", "sfbay", "washingtondc", "cnj"};
+        String maxPrice = "10000";
+
+        for (String s : cityArray) {
+            for (String value : modelArray) {
+                String modelWithOutWhitespace = value.replaceAll(" ", "%20");
+                String nameDomain = "https://" + s + ".craigslist.org/d/cars-trucks-by-owner/search/cto?auto_make_model=";
+                URL aUrl = new URL(nameDomain + modelWithOutWhitespace + "&max_price=" + maxPrice);
+                int adsCurrent = 0;
+
+                try (BufferedReader in = new BufferedReader(new InputStreamReader(aUrl.openStream()))) {
+                    String line;
+
+                    while ((line = in.readLine()) != null) {
+                        if (line.indexOf("<span class=\"result-meta\">") > 0) {
+                            if ((line = in.readLine()) != null) {
+                                int startIndexPrice = line.indexOf("result-price");
+
+                                if (startIndexPrice != -1) {
+                                    adsCurrent++;
+                                    int endIndexPrice = line.indexOf("</", startIndexPrice);
+                                    System.out.println(value + " #" + adsCurrent + " price : " + line.substring(startIndexPrice + 14, endIndexPrice));
+                                }
+                            }
+                        }
+                    }
+
+                    System.out.println("Number of ads " + value + " in " + s + " and area: " + adsCurrent);
+                }
+            }
+        }
+    }
+}
